b4edb3de8f74f7abde2b9baef11677e5013cf1ef
Beatriz Beraldo <biaberaldo@outlook.com>
2023-09-12 19:25:25 -0300

    fix forms

--- src/components/common/Form/Login.tsx
+++ src/components/common/Form/Login.tsx
@@ -8,101 +8,107 @@ import { useForm } from "react-hook-form";
 import { useState, Fragment } from "react";
 
 type Inputs = {
-  nome: string;
-  email: string;
+	nome: string;
+	email: string;
 };
 
 const FormLogin = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<Inputs>();
-  let [isOpen, setIsOpen] = useState(false);
-  const [mouse, setMouse] = useState("");
-  const [telefone, setTelefone] = useState("");
-  const [msgPP, setMsgPP] = useState("hidden");
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm<Inputs>();
+	let [isOpen, setIsOpen] = useState(false);
+	const [mouse, setMouse] = useState("");
+	const [telefone, setTelefone] = useState("");
+	const [msgPP, setMsgPP] = useState("hidden");
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+	function closeModal() {
+		setIsOpen(false);
+	}
 
-  function openModal() {
-    setIsOpen(true);
-  }
+	function openModal() {
+		setIsOpen(true);
+	}
 
-  return (
-    <form className="flex flex-col gap-y-3 max-w-xl w-full justify-center font-sphere">
-      <div className="flex flex-col">
-        <label htmlFor="" className={`text-brand-200 text-sm `}>
-          Nome:
-        </label>
-        <div className={`relative `}>
-          <div
-            className={`absolute w-10 inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-brand-200 z-10 text-lg ${
-              errors.nome && "text-brand-200"
-            } transition duration-500`}
-          >
-            <FaUser />
-          </div>
-          <input
-            placeholder="Seu nome."
-            type="text"
-            {...register("nome", {
-              required: "Nome é obrigatório",
-            })}
-            className="py-2 pl-10 pr-3 bg-zinc-50 outline-none rounded-lg relative w-full"
-          />
-        </div>
-      </div>
+	return (
+		<form className="flex flex-col gap-y-3 max-w-xl w-full justify-center font-sphere">
+			<div className="flex flex-col">
+				<label
+					htmlFor=""
+					className={`text-brand-200 text-sm `}
+				>
+					Nome:
+				</label>
+				<div className={`relative `}>
+					<div
+						className={`absolute w-10 inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-brand-200 z-10 text-lg ${
+							errors.nome && "text-brand-200"
+						} transition duration-500`}
+					>
+						<FaUser />
+					</div>
+					<input
+						placeholder="Seu nome."
+						type="text"
+						{...register("nome", {
+							required: "Nome é obrigatório",
+						})}
+						className="py-2 pl-10 pr-3 bg-zinc-50 outline-none rounded-lg relative w-full"
+					/>
+				</div>
+			</div>
 
-      <div className="flex flex-col">
-        <label htmlFor="" className={`text-brand-200 text-sm `}>
-          Senha:
-        </label>
-        <div className={`relative `}>
-          <div
-            className={`absolute w-10 inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-brand-200 z-10 text-lg ${
-              errors.nome && "text-brand-200"
-            } transition duration-500`}
-          >
-            <FaUser />
-          </div>
-          <input
-            placeholder="Seu nome."
-            type="text"
-            {...register("nome", {
-              required: "Nome é obrigatório",
-            })}
-            className="py-2 pl-10 pr-3 bg-zinc-50 outline-none rounded-lg relative w-full"
-          />
-        </div>
-      </div>
-      <p className="text-brand-200 font-medium">
-        Não tem cadastro?{" "}
-        <a href="/cadastro" className="underline">
-          Cadastre-se!
-        </a>
-      </p>
+			<div className="flex flex-col">
+				<label
+					htmlFor=""
+					className={`text-brand-200 text-sm `}
+				>
+					Senha:
+				</label>
+				<div className={`relative `}>
+					<div
+						className={`absolute w-10 inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-brand-200 z-10 text-lg ${
+							errors.nome && "text-brand-200"
+						} transition duration-500`}
+					>
+						<FaUser />
+					</div>
+					<input
+						placeholder="Sua senha."
+						type="text"
+						className="py-2 pl-10 pr-3 bg-zinc-50 outline-none rounded-lg relative w-full"
+					/>
+				</div>
+			</div>
+			<p className="text-brand-200 font-medium">
+				Não tem cadastro?{" "}
+				<a
+					href="/cadastro"
+					className="underline"
+				>
+					Cadastre-se!
+				</a>
+			</p>
 
-      {Object.keys(errors).length !== 0 && (
-        <p className="text-white text-sm">
-          Os campos obrigatórios estão em vermelho.
-        </p>
-      )}
-      <div className="flex place-content-center">
-        <button className="relative inline-flex items-center justify-center px-6 py-2 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group">
-          <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-brand-400 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
-          <span className="absolute inset-0 w-full h-full bg-brand-200 rounded-md border border-brand-100"></span>
-          <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-brand-100 rounded-md opacity-0 group-hover:opacity-100 "></span>
-          <span className="relative text-brand-100 transition-colors duration-200 ease-in-out delay-100 group-hover:text-brand-200">
-            Enviar
-          </span>
-        </button>
-      </div>
-    </form>
-  );
+			{Object.keys(errors).length !== 0 && (
+				<p className="text-white text-sm">
+					Os campos obrigatórios estão em vermelho.
+				</p>
+			)}
+			<div className="flex place-content-center">
+				<button className="relative inline-flex items-center justify-center px-6 py-2 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group">
+					<span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-brand-400 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+					<span className="absolute inset-0 w-full h-full bg-brand-200 rounded-md border border-brand-100"></span>
+					<span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-brand-100 rounded-md opacity-0 group-hover:opacity-100 "></span>
+					<span className="relative text-brand-100 transition-colors duration-200 ease-in-out delay-100 group-hover:text-brand-200">
+						Enviar
+					</span>
+				</button>
+			</div>
+		</form>
+	);
 };
 
 export default FormLogin;
